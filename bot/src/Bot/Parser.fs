a09e01384fa073873c9bd596b428cd375fae56d5
palutz <stefano.paluello@gmail.com>
2017-05-09 19:01:54 +0100

    usign Eq active pattern

--- bot/src/Bot/Parser.fs
+++ bot/src/Bot/Parser.fs
@@ -3,11 +3,12 @@ namespace fintechHouse.Bot
 module Parser = 
 
   open Domain
+  open Utility
 
   let parseTweet (str: string) = 
     let x = str.Split([|' '|])
     match x.Length with 
-      | 2 -> 
+      | Eq 2 -> 
           Ok (
             {
               Sender = x.[0]
