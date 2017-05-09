d09cdb1c21cd4a97c5a28324dd58448f0f7c402c
palutz <stefano.paluello@gmail.com>
2017-05-09 18:00:40 +0100

    remove tryparseExact

--- bot/src/Bot/Utility.fs
+++ bot/src/Bot/Utility.fs
@@ -19,21 +19,15 @@ module Utility =
       | true, rs -> Some rs
       | false, _ -> None
 
-  let (|TryParseExactAP|_|) f (format: String) v =
-    match f (v, format, CultureInfo.InvariantCulture, DateTimeStyles.None) with
-      | true, rs -> Some rs
-      | false, _ -> None
+  // let (|TryParseExactAP|_|) f format v =
+  //   match f (v, format, CultureInfo.InvariantCulture, DateTimeStyles.None) with
+  //     | true, rs -> Some rs
+  //     | false, _ -> None
 
 
-let aa = DateTime.TryParseExact (" ", "format", CultureInfo.InvariantCulture, DateTimeStyles.None) 
+// let aa = DateTime.TryParseExact ("01/01/2000", "M/dd/yyyy", CultureInfo.InvariantCulture, DateTimeStyles.None)  
 
-match "1/1/2000" with
-| TryParseAP DateTime.TryParse x -> Some(x)
-| _ -> None
-
-
-match "1/1/2000" with
-  | TryParseExactAP DateTime.TryParseExact format) x -> Some ()
-  | _ -> None
-
+// match "1/1/2000" with
+// | TryParseAP DateTime.TryParse x -> Some(x)
+// | _ -> None
 
