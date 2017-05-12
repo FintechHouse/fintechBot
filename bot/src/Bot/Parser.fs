3e011ca23666819fce157698de72dc7427250f06
palutz <stefano.paluello@gmail.com>
2017-05-12 19:58:06 +0100

    clean up

--- bot/src/Bot/Parser.fs
+++ bot/src/Bot/Parser.fs
@@ -10,25 +10,3 @@ module Parser =
     match str with 
       | TryParseQuery q -> Ok(q)
       | _ -> Error "not a correct query"
-
-
-      //let x = str.Split([|' '|])
-    //match x with 
-      // ask the quotation for all the year
-      // | [| snd; sy; TryParseIntoYear Int32.TryParse yy |] ->
-      //     Ok (
-      //       {
-      //         Sender = x.[0]
-      //         Symbol = x.[1]
-      //         FromDate = DateTime(yy, 01, 01)
-      //         ToDate = DateTime(yy, 12, 31)
-      //       }) 
-      // | [| snd; sy; TryParseAP DateTime.TryParse startD; TryParseAP DateTime.TryParse endD |] ->
-      //     Ok (
-      //       {
-      //         Sender = x.[0]
-      //         Symbol = x.[1]
-      //         FromDate = startD
-      //         ToDate = endD
-      //       }) 
-
