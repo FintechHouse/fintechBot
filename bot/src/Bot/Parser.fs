456f85064cc181d32b8c2392ce287a3c1e75cabc
palutz <stefano.paluello@gmail.com>
2017-05-12 19:57:10 +0100

    simplified parser with AP on the domain

--- bot/src/Bot/Parser.fs
+++ bot/src/Bot/Parser.fs
@@ -7,24 +7,28 @@ module Parser =
   open Utility
 
   let parseTweet (str: string) = 
-    let x = str.Split([|' '|])
-    match x with 
-      // ask the quotation for all the year
-      | [| snd; sy; TryParseAP Int32.TryParse y & TryYear yy |] ->
-          Ok (
-            {
-              Sender = x.[0]
-              Symbol = x.[1]
-              FromDate = DateTime(yy, 01, 01)
-              ToDate = DateTime(yy, 12, 31)
-            }) 
-      | [| snd; sy; TryParseAP DateTime.TryParse startD; TryParseAP DateTime.TryParse endD |] ->
-          Ok (
-            {
-              Sender = x.[0]
-              Symbol = x.[1]
-              FromDate = startD
-              ToDate = endD
-            }) 
+    match str with 
+      | TryParseQuery q -> Ok(q)
       | _ -> Error "not a correct query"
+
+
+      //let x = str.Split([|' '|])
+    //match x with 
+      // ask the quotation for all the year
+      // | [| snd; sy; TryParseIntoYear Int32.TryParse yy |] ->
+      //     Ok (
+      //       {
+      //         Sender = x.[0]
+      //         Symbol = x.[1]
+      //         FromDate = DateTime(yy, 01, 01)
+      //         ToDate = DateTime(yy, 12, 31)
+      //       }) 
+      // | [| snd; sy; TryParseAP DateTime.TryParse startD; TryParseAP DateTime.TryParse endD |] ->
+      //     Ok (
+      //       {
+      //         Sender = x.[0]
+      //         Symbol = x.[1]
+      //         FromDate = startD
+      //         ToDate = endD
+      //       }) 
 
