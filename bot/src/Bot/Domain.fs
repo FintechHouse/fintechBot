72ae1df5293eda82c9c89d14d5b3611901ebf355
palutz <stefano.paluello@gmail.com>
2017-05-12 19:56:31 +0100

    Domain tryparse active pattern

--- bot/src/Bot/Domain.fs
+++ bot/src/Bot/Domain.fs
@@ -3,6 +3,7 @@ namespace fintechHouse.Bot
 module Domain = 
 
   open System
+  open Utility
 
   type Query = {
     Sender : string
@@ -10,3 +11,36 @@ module Domain =
     FromDate: DateTime
     ToDate: DateTime
   }
+
+  type QueryWithYear = {
+    Sender : string
+    Symbol : string
+    Year: int
+  }
+
+  let (|TryParseQuery|_|) (q: string) =
+    let ar = q.Split([|' '|])
+    match ar with
+      | [| s; sn; TryParseAP DateTime.TryParse fd; TryParseAP DateTime.TryParse td; |] -> 
+          Some(
+            {
+              Sender = s
+              Symbol = sn
+              FromDate = fd
+              ToDate = td
+            })
+      | _ -> None
+
+  
+  let (|TryParseQueryYear|_|) (q: string) =
+    let ar = q.Split([|' '|])
+    match ar with
+      | [| s; sn; fd; td; |] -> 
+          Some(
+            {
+              Sender = s
+              Symbol = sn
+              Year = 2017
+            })
+      | _ -> None
+
