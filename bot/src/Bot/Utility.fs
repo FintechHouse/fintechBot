2400d4eed8fe67855e7de2f49f1a6d8e54a03484
palutz <stefano.paluello@gmail.com>
2017-05-13 10:41:22 +0100

    datetime AP parser utilities

--- bot/src/Bot/Utility.fs
+++ bot/src/Bot/Utility.fs
@@ -3,7 +3,6 @@ namespace fintechHouse.Bot
 
 module Utility =
   open System
-  open System.Globalization
 
   let (|Eq|_|) expect myval =    // check equality on any type supporting equality
     if expect = myval then Some ()
@@ -27,7 +26,9 @@ module Utility =
       | true, rs -> Some rs
       | false, _ -> None
 
-  
+  // ************************************************************************
+  //  DateTime active pattern parser utilities
+  // ************************************************************************
   let nextYear = DateTime.Now.Year + 1
 
   let (|IntToYear|_|) y = 
@@ -41,15 +42,18 @@ module Utility =
     | true, IntToYear rs -> Some rs
     | _, _ -> None
 
-  // let (|TryParseExactAP|_|) f format v =
-  //   match f (v, format, CultureInfo.InvariantCulture, DateTimeStyles.None) with
-  //     | true, rs -> Some rs
-  //     | false, _ -> None
+  let dateStyleNone = Globalization.DateTimeStyles.None
+  let cultureInv = Globalization.CultureInfo.InvariantCulture
+
+  let(|TryParseDateFrmt|_|) (frmt: String) (d : String)  =
+    match DateTime.TryParseExact(d, frmt, cultureInv, dateStyleNone) with
+    | true, res -> Some res
+    | false, _ -> None
 
 
 // let aa = DateTime.TryParseExact ("01/01/2000", "M/dd/yyyy", CultureInfo.InvariantCulture, DateTimeStyles.None)  
 
 // match "1/1/2000" with
-// | TryParseAP DateTime.TryParse x -> Some(x)
-// | _ -> None
+//  | TryParseDateFrmt "yyyy-MM-dd" x -> Some(x)
+//  | _ -> None
 
